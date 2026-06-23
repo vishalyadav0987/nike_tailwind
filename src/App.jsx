d7267e392a8f548b5c79c0990febd82b7a32bf48
vishal <vishal.yadav@myfindoc.com>
2026-06-23 15:30:05 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,9 @@ const App = () => {
         <section className="padding-x sm:py-32 py-16 w-full">
           <Subscribes/>
         </section>
-        {/* <section className="bg-black padding-x padding-t pb-8">
+        <section className="bg-black padding-x padding-t pb-8">
           <Footer/>
-        </section> */}
+        </section>
       </main>
     </>
   )
